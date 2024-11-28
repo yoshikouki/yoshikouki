4ff38f0b13dd263fc6e5680c29e1c84f5295c2c4
yoshikouki <yoshikouki@gmail.com>
2024-11-28 23:18:32 +0900

    Clean code

--- apps/add-inputs/src/index.ts
+++ apps/add-inputs/src/index.ts
@@ -1,15 +1,9 @@
 import { storeUrl } from "./store";
 import { getUrl } from "./url";
 
-const getTodayString = (): string => {
-  const currentDate = new Date();
-  const formattedDate = `${currentDate.getFullYear()}/${currentDate.getMonth() + 1}/${currentDate.getDate()}`;
-  return formattedDate;
+const main = async () => {
+  const url = getUrl();
+  await storeUrl(url);
 };
 
-const url = getUrl();
-await storeUrl(url);
-// const content = await getContent(url);
-// const today = getTodayString();
-// const output = `- [${content}](${url}): ${today}`;
-// console.log(output);
+await main();
